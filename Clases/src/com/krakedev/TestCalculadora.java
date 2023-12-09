45aa65be2f8e87cf55dd6f7a12fc643c98faeb3b
SandraIpiales <seipiales@uce.edu.ec>
2023-12-09 15:26:23 -0500

    se agrego la funcion Multiplicar Funcional

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -3,7 +3,19 @@ package com.krakedev;
 public class TestCalculadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Calculadora calcu=new Calculadora();
+		int resultadoSuma;
+		double resultadoResta;
+		double resultadoMultiplicar;
+		
+		resultadoSuma = calcu.sumar(5,3);
+		resultadoResta=calcu.restar(10, 2);
+		resultadoMultiplicar=calcu.multiplicar(10,5);
+	
+		System.out.println("RESULTADO SUMA: "+resultadoSuma);
+		System.out.println("RESULTADO RESTA: "+resultadoResta);
+		System.out.println("RESULTADO MULTIPLICACION: "+resultadoMultiplicar);
+
 
 	}
 
