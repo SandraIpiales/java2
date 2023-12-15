5a2665c8e7c427f60e4bbf74280e806e3bb55337
SandraIpiales <seipiales@uce.edu.ec>
2023-12-14 21:37:57 -0500

    se agrego Ejercicio 4 Funcional

---
+++ Evaluacion1SandraIpiales/src/com/clearminds/maquina/MaquinaDulces.java
@@ -0,0 +1,41 @@
+package com.clearminds.maquina;
+
+import java.util.ArrayList;
+
+import com.clearminds.componentes.Celda;
+
+public class MaquinaDulces {
+	private ArrayList<Celda> celdas;
+	private double saldo;
+	
+	public MaquinaDulces() {
+		celdas= new ArrayList<Celda>();
+	}
+	
+	public void agregarCelda(Celda celdaA) {
+		celdas.add(celdaA);
+	}
+	public void mostrarConfiguracion() {
+		Celda elementoCelda;
+		for(int i=0;i<celdas.size();i++) {
+			elementoCelda= celdas.get(i);
+			System.out.println("Celda: " + elementoCelda.getCodigo());
+		}
+
+	} 
+	public ArrayList<Celda> getCeldas() {
+		return celdas;
+	}
+	public void setCeldas(ArrayList<Celda> celdas) {
+		this.celdas = celdas;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+	
+}
