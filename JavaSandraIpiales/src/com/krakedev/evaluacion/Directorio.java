8109cb37d4470beec05a58bb5a81b914ca5b0391
SandraIpiales <seipiales@uce.edu.ec>
2023-12-18 20:50:51 -0500

    se agrego la clase TestUltimaModificacion Funcional

--- JavaSandraIpiales/src/com/krakedev/evaluacion/Directorio.java
+++ JavaSandraIpiales/src/com/krakedev/evaluacion/Directorio.java
@@ -1,61 +1,71 @@
 package com.krakedev.evaluacion;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Directorio {
 	private ArrayList<Contacto> contactos;
 	private Date fechaModificacion;
-	
+
 	public Directorio() {
 		contactos = new ArrayList<Contacto>();
 		fechaModificacion = new java.util.Date();
 	}
-	
+
 	public ArrayList<Contacto> getContactos() {
 		return contactos;
 	}
+
 	public void setContactos(ArrayList<Contacto> contactos) {
 		this.contactos = contactos;
 	}
+
 	public Date getFechaModificacion() {
 		return fechaModificacion;
 	}
+
 	public void setFechaModificacion(Date fechaModificacion) {
 		this.fechaModificacion = fechaModificacion;
 	}
-	
-	public boolean agregarContacto(Contacto contactoA) {	
+
+	public boolean agregarContacto(Contacto contactoA) {
 		Contacto elementoContacto, contactoEncontrado;
-		String cedulaIngresada=contactoA.getCedula();
+		String cedulaIngresada = contactoA.getCedula();
 		boolean contactoApto;
-		if(contactos!=null) {
-			contactoEncontrado=buscarPorCedula(cedulaIngresada);
-			if(contactoEncontrado!=null) {
-				contactoApto=false;
-			}else {
-				contactoApto=true;
+		if (contactos != null) {
+			contactoEncontrado = buscarPorCedula(cedulaIngresada);
+			if (contactoEncontrado != null) {
+				contactoApto = false;
+			} else {
+				contactoApto = true;
 				contactos.add(contactoA);
 			}
-		}else {
+		} else {
 			contactos.add(contactoA);
-			contactoApto= true;
+			contactoApto = true;
 		}
 		return contactoApto;
-	
+
 	}
+
 	public Contacto buscarPorCedula(String cedula) {
 		Contacto elementoContacto;
-		Contacto contactoEncontrado=null;
+		Contacto contactoEncontrado = null;
 		String cedulaBuscada;
-		for(int i=0;i<contactos.size();i++) {
-			elementoContacto=contactos.get(i);
-			cedulaBuscada=elementoContacto.getCedula();
-			if(cedulaBuscada.equals(cedula)) {
+		for (int i = 0; i < contactos.size(); i++) {
+			elementoContacto = contactos.get(i);
+			cedulaBuscada = elementoContacto.getCedula();
+			if (cedulaBuscada.equals(cedula)) {
 				contactoEncontrado = elementoContacto;
 			}
 		}
 		return contactoEncontrado;
 	}
-	
+	public String consultarUltimaModificacion() {
+		 SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		 String datoModificado=formato.format(fechaModificacion);
+		return datoModificado;
+	}
+
 }
